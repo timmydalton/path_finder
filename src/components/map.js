eeb159b2ff59675b3dd4f92dd060eae4c97ffd1b
KhanhDuyTr <timmydalton2k@gmail.com>
2023-12-19 10:55:12 +0700

    [feat] init map

---
+++ src/components/map.js
@@ -0,0 +1,25 @@
+import { useRef, useState, useEffect } from "react";
+
+function Map() {
+  const mapContainer = useRef();
+  const [map, setMap] = useState({});
+
+  useEffect(()=>{
+    const L = window.L
+    const map = L.map(mapContainer.current, {attributionControl: false}).setView([21.0224683, 105.8203064], 16);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // unmount map function
+    return () => map.remove();
+  }, []);
+
+  return (
+      <div style={{padding: 0, margin: 0, width: "100%", height: "100vh",}}
+            ref={el => mapContainer.current = el}>
+      </div>
+  );
+}
+
+export default Map
