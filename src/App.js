7a08b2859cb13933c5b87f48804936aacb10fd51
KhanhDuyTr <timmydalton2k@gmail.com>
2023-12-20 13:50:07 +0700

    [super update]

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { useRef, useState, useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
+import { LeafletMouseEvent, LatLng } from "leaflet";
+import { Map, Marker, TileLayer, ZoomControl } from "react-leaflet";
+import { markerA, markerB } from "./Icons";
 import { findPath } from "./algorithm";
 import geoData from "./data/o_cho_dua.json"
 import geoDataParsed from "./data/o_cho_dua_new.json"
@@ -7,64 +10,121 @@ import './App.css';
 function App() {
   const mapContainer = useRef();
 
-  useEffect(()=>{
-    const L = window.L
-    const map = L.map(mapContainer.current, {attributionControl: false}).setView([21.0224683, 105.8203064], 16);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+  const [lng, setLng] = useState(105.8203064);
+  const [lat, setLat] = useState(21.0224683);
+  const [zoom, setZoom] = useState(16);
 
-    console.log("map data", geoData)
-    const parsing = geoData
-    parsing.features = geoData.features.filter(el => {
-      return ['Point', 'LineString'].includes(el.geometry.type) && el.properties.highway
-    })
+  // start and end markers
+  const [startNode, setStartNode] = useState(null); //string
+  const [endNode, setEndNode] = useState(null); //string
+  const [startMarkerPos, setStartMarkerPos] = useState(null); //LatLng
+  const [endMarkerPos, setEndMarkerPos] = useState(null); //LatLng
+  const startNodeMarker = useRef(null); //Marker
+  const endNodeMarker = useRef(null); //Marker
 
-    console.log("parsing data", parsing)
-    
-    let text = ''
+  // pathfinding state
+  const [pathFound, setPathFound] = useState(false); //Bool
+  const [isRunning, setIsRunning] = useState(false); //Bool
 
-    console.log("parsed geo data", geoDataParsed)
+  // final pathfinding path
+  const [path, setPath] = useState([]); //Array of LatLng
 
-    const point = geoDataParsed.features.filter(el => el.geometry.type == 'Point')
-    console.log("point", point)
-    const path = geoDataParsed.features.filter(el => el.geometry.type != 'Point')
-    console.log("path", path)
+  const findClosestNode = (latlng) => {
+    const lat = latlng.lat;
+    const lon = latlng.lng;
+    return {
+      key: `${lat}`,
+      lat,
+      lon
+    };
+  };
 
-    function onMapClick(e) {
-      let text = ''
-      if (!window.markerA) text = 'A'
-      else if (!window.markerB) text = 'B'
-      else {
-        map.removeLayer(window.markerA)
-        map.removeLayer(window.markerB)
-        window.markerA = null
-        window.markerB = null
-        return
+  const handleClick = (e) => {
+    if (!startNode || !endNode) {
+      const closest = findClosestNode(e.latlng);
+      if (closest) {
+        if (!startNode) {
+          setStartNode(closest.key);
+          setStartMarkerPos(new LatLng(closest.lat, closest.lon));
+        } else {
+          setEndNode(closest.key);
+          setEndMarkerPos(new LatLng(closest.lat, closest.lon));
+        }
       }
-
-      console.log(e.latlng)
-
-      const newMarker = L.marker(e.latlng, {
-        icon: L.divIcon({
-          iconSize: 'auto',
-          className: 'icon-marker',
-          html: `<div class="marker-text">${text}</div>`
-        })
-      }).addTo(map)
-
-      if (text == 'A') window.markerA = newMarker
-      if (text == 'B') window.markerB = newMarker
     }
+  }
 
-    map.on('click', onMapClick)
+  //Drag node
+  const onStartNodeDrag = async (e) => {};
 
-    // unmount map function
-    return () => map.remove();
-  }, []);
+  const onEndNodeDrag = async (e) => {};
+
+  // on finish drag, set position to nearest
+  const onStartNodeDragEnd = (e) => {
+    const closest = findClosestNode(e.target._latlng);
+    if (closest) {
+      setStartNode(closest.key);
+      setStartMarkerPos(new LatLng(closest.lat, closest.lon));
+    }
+  };
+
+  const onEndNodeDragEnd = (e) => {
+    const closest = findClosestNode(e.target._latlng);
+    if (closest) {
+      setEndNode(closest.key);
+      setEndMarkerPos(new LatLng(closest.lat, closest.lon));
+    }
+  };
+
   return (
-    <div style={{padding: 0, margin: 0, width: "100%", height: "100vh",}}
-      ref={el => mapContainer.current = el}>
+    <div className="App">
+      <Map
+        preferCanvas
+        center={[lat, lng]}
+        zoom={zoom}
+        zoomControl={false}
+        onClick={handleClick}
+      >
+        <TileLayer
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+
+        <ZoomControl position={"bottomleft"} />
+
+        {/* <PathfindingMarkers nodeData={nodeData} nodes={nodes} /> */}
+
+        {/* Render start/end markers */}
+        {startMarkerPos && (
+          <Marker
+            ref={startNodeMarker}
+            position={[startMarkerPos.lat, startMarkerPos.lng]}
+            icon={markerA}
+            draggable
+            ondrag={onStartNodeDrag}
+            ondragend={onStartNodeDragEnd}
+          />
+        )}
+
+        {endMarkerPos && (
+          <Marker
+            ref={endNodeMarker}
+            position={[endMarkerPos.lat, endMarkerPos.lng]}
+            icon={markerB}
+            draggable
+            ondragstart={() => {
+              setPath([]);
+            }}
+            ondrag={onEndNodeDrag}
+            ondragend={onEndNodeDragEnd}
+          />
+        )}
+
+        {/* Render final path, if exists
+        {/* {pathFound && path.length > 0 && (
+          <AnimatedPolyline positions={path} snakeSpeed={300} />
+        )} */}
+      </Map>
     </div>
   );
 }
---
+++ src/Icons.js
@@ -0,0 +1,16 @@
+import { Icon } from "leaflet";
+
+export const markerA = new Icon({
+  iconUrl: "/markerA.svg",
+  iconSize: [32, 32],
+});
+
+export const markerB = new Icon({
+  iconUrl: "/markerB.svg",
+  iconSize: [32, 32],
+});
+
+export const nodeMarker = new Icon({
+  iconUrl: "/small.png",
+  iconSize: [8, 8],
+});
