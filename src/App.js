956cf47b2ca01c4cef0f1e4d90d915f339337f03
KhanhDuyTr <timmydalton2k@gmail.com>
2023-12-20 11:13:30 +0700

    [improve]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import logo from './logo.svg';
+import { useRef, useState, useEffect } from "react";
+import geoData from "./data/o_cho_dua.json"
 import './App.css';
-import Map from './components/map';
 
 function App() {
+  const mapContainer = useRef();
+
+  useEffect(()=>{
+    const L = window.L
+    const map = L.map(mapContainer.current, {attributionControl: false}).setView([21.0224683, 105.8203064], 16);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    console.log("map data", geoData)
+    
+    let text = ''
+    const geoDat = geoData
+    geoDat.features = geoDat.features.filter(el => ['Point', 'LineString'].includes(el.geometry.type) && el.properties.highway)
+    console.log("path and point data", geoDat)
+
+    function onMapClick(e) {
+      let text = ''
+      if (!window.markerA) text = 'A'
+      else if (!window.markerB) text = 'B'
+      else {
+        map.removeLayer(window.markerA)
+        map.removeLayer(window.markerB)
+        window.markerA = null
+        window.markerB = null
+        return
+      }
+
+      console.log(e.latlng)
+
+      const newMarker = L.marker(e.latlng, {
+        icon: L.divIcon({
+          iconSize: 'auto',
+          className: 'icon-marker',
+          html: `<div class="marker-text">${text}</div>`
+        })
+      }).addTo(map)
+
+      if (text == 'A') window.markerA = newMarker
+      if (text == 'B') window.markerB = newMarker
+    }
+
+    map.on('click', onMapClick)
+
+    // unmount map function
+    return () => map.remove();
+  }, []);
   return (
-    <div>
-      <Map/>
+    <div style={{padding: 0, margin: 0, width: "100%", height: "100vh",}}
+      ref={el => mapContainer.current = el}>
     </div>
   );
 }
